8eb65774d6bcaeba31680cd5fff650e3b0ae6c1b
Milan Urukalo <urukalo@gmail.com>
2024-09-29 18:56:44 +0200

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Add interactivity for smooth scrolling or other features
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
